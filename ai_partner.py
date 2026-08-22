d7c1b6c30db81af1ee21a0f0626184a7497d7386
xy <xy>
2026-08-22 15:36:50 +0800

    update

--- ai_partner.py
+++ ai_partner.py
@@ -2,74 +2,45 @@ import streamlit as st
 import os
 from openai import OpenAI
 
+st.title("DeepSeek-like clone")
 
-# 配置 Streamlit 页面
-st.set_page_config(
-    page_title="AI伴侣",
-    page_icon="😘",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': 'https://www.extremelycoolapp.com/bug',
-        'About': '# This is a header. This is an *extremely* cool app!'
-    }
-)
+system_prompt = "You are a helpful assistant"
 
-# 设置页面标题和 Logo
-st.title("AI伴侣")
-st.logo("resource/logo.png")
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com")    
 
-# 设定 AI 的角色
-system_prompt = "你是我的女朋友，可爱且超级喜欢我"
-
-# 初始化当前会话的聊天记录
+# 初始化对话记录并防止重复初始化
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# 显示历史消息
+# 显示之前的对话记录
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+if prompt := st.chat_input("Say something"):
+    # 显示用户消息
+    st.chat_message("user").markdown(prompt)
 
-# 创建 DeepSeek 客户端
-prompt = st.chat_input("Say something")
+    # 添加用户消息到对话记录
+    st.session_state.messages.append({"role" : "user", "content" : prompt})
 
-client = OpenAI(
-    api_key=os.environ.get('DEEPSEEK_API_KEY'),
-    base_url="https://api.deepseek.com")
+    #机器人的消息处理
+    with st.chat_message("ai"):
+        stream = client.chat.completions.create(
+            model="deepseek-v4-flash",
+            messages=[
+                {"role": "system", "content": system_prompt},
+                *st.session_state.messages
+            ],
+            stream=True,
+            reasoning_effort="high",
+            extra_body={"thinking": {"type": "enabled"}}
+            )
 
-if prompt:
-    # 显示并保存用户消息
-    st.chat_message("user").write(prompt)
-    print("提示词", prompt)
+        response = st.write_stream(stream)
 
-    st.session_state.messages.append({
-        "role": "user",
-        "content": prompt
-    })
+    st.session_state.messages.append({"role" : "assistant", "content" : response})
 
-    # 请求模型生成回复
-    response = client.chat.completions.create(
-    model="deepseek-v4-flash",
-    messages=[
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": prompt},
-    ],
-    stream=False,
-    reasoning_effort="high",
-    extra_body={"thinking": {"type": "enabled"}}
-    )
 
-    answer = response.choices[0].message.content
-    
-    print("LLM返回的结果:", answer)
-
-    # 显示并保存 AI 回复
-    st.chat_message("assistant").write(answer)
-
-    st.session_state.messages.append({
-        "role": "assistant",
-        "content": answer
-    })
